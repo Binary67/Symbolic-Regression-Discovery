b0cdc1bd0a60c157c72f9e64f03c90207fdae6f5
Franky Lau <frankylau96@hotmail.com>
2025-05-18 00:04:12 +0800

    Add genetic programming main script and modify data generation

--- DataGeneration.py
+++ DataGeneration.py
@@ -1,11 +1,10 @@
 import numpy as np
-import pandas as pd
 
 def GenerateDummyData(SampleSize, NoiseSigma):
   ActualLaw = lambda x: 3*np.sin(x) + 0.5*x**2
   
   X = np.linspace(-4, 4, SampleSize)
   Y = ActualLaw(X) + np.random.normal(0, NoiseSigma, SampleSize)
-  df = pd.DataFrame({"X": X, "Y": Y})
+  Data = {"X": X, "Y": Y}
 
-  return df
+  return Data
